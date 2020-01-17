0bcdec38b40e3b5a30264b99467cbc53c813fa09
kzl-000 <46276302+kzl-000@users.noreply.github.com>
2020-01-17 22:09:48 +0800

    Update Menu.cpp

--- MiniMC/Menu.cpp
+++ MiniMC/Menu.cpp
@@ -1,33 +1,83 @@
 #include "Menu.h"
 #include <iostream>
 #include "glut.h"
+
+GLfloat verticesm[] = {
+	//rectangle
+	-1.0f, 0.8f, 0.0f,
+	-0.8f, 0.8f, 0.0f,
+	-1.0f, 1.0f, 0.0f,
+	-0.8f, 1.0f, 0.0f,
+	1.0f, 0.8f, 0.0f,
+	0.8f, 0.8f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+	0.8f, 1.0f, 0.0f,
+
+	//cancle
+	-0.935, 0.78, 0,
+	-0.96, 0.75, 0,
+	-0.835, 0.65, 0,
+	-0.86, 0.625, 0,
+	-0.86, 0.78, 0,
+	-0.835, 0.75, 0,
+	-0.96, 0.65, 0,
+	-0.935, 0.625, 0,
+
+	//pause
+	0.86, 0.77, 0,
+	0.89, 0.77, 0,
+	0.86, 0.65, 0,
+	0.89, 0.65, 0,
+	0.86 + 0.05, 0.77, 0,
+	0.89 + 0.05, 0.77, 0,
+	0.86 + 0.05, 0.65, 0,
+	0.89 + 0.05, 0.65, 0
+};
+
+GLuint indicesm[] = {
+		0,1,2,
+		1,2,3,
+
+		4,5,6,
+		5,6,7,
+
+		8,9,10,
+		9,10,11,
+
+		12,13,14,
+		13,14,15,
+
+		16,17,18,
+		17,18,19,
+
+		20,21,22,
+		21,22,23,
+
+		24, 25, 26,
+		25,26,27
+};
+
+GLuint VAO2, VBO2, EBO2;
+
 void Menu::draw()
 {
 	shader->use();
-	glm::mat4 ortho(1.f), view(1.f), model(1.f);
-	view = glm::lookAt(glm::vec3{ 0.0, 0.0, 100 }, { 0.0, 0.0, 0.0 }, { 0.0, 1.0, 0.0 });
-	ortho = glm::ortho(-50, 50, -50, 50, -500, 500);
-	ortho = glm::perspective(glm::radians(45.f), 1.f, 0.1f, 100.f);
-	glOrtho(-50, 50, -50, 50, -500, 500);
-	glm::vec3 pos = glm::vec3(20, 20, 50);
-	model = glm::translate(model, pos);
-	glTranslatef(25, 80, 50);
-	shader->setInt("a", 0.5);
-	shader->setInt("s", 0.8);
-	shader->setMat4("ortho", ortho);
-	shader->setMat4("view", view);
-	shader->setMat4("model", model);
-	if (press[0])
-		glutSolidCube(8);
-	else
-		glutSolidCube(10);
-	pos = glm::vec3(-40, 0, 0);
-	model = glm::translate(model, pos);
-	shader->setMat4("model", model);
-	if (press[1])
-		glutSolidCube(8);
-	else
-		glutSolidCube(10);
+	glGenBuffers(1, &VBO2);
+	glGenBuffers(1, &EBO2);
+	glGenVertexArrays(1, &VAO2);
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesm), verticesm, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesm), indicesm, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(VAO2);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+
 }
 
 int Menu::changepress(float x, float y)
@@ -38,14 +88,14 @@ int Menu::changepress(float x, float y)
 		paused = !paused;
 		return 0;
 	}
-	if (y < 480 && y > 410 && !paused)
-		if (x > 600 && x < 720)
+	if (y < 480 && y > 432 && !paused)
+		if (x > 648 && x < 720)
 		{
 			press[0] = true;
 			paused = !paused;
 			return 0;
 		}
-		else if (x > 0 && x < 120)
+		else if (x > 0 && x < 72)
 		{
 			press[1] = true;
 			return 1;
